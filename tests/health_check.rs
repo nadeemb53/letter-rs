b6b468ad7cdc0384d9fc963f03cf5db8dc0fd96a
nadeemb53 <nadeemb53@gmail.com>
2022-08-22 15:01:53 +0530

    integration test

---
+++ tests/health_check.rs
@@ -0,0 +1,15 @@
+#[tokio::test]
+async fn health_check_works(){
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client.get("http://127.0.0.1:8000/health_check").send().await.expect("Failed to send request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app(){
+    let server = zero2prod::run().expect("Failed to spawn our app.");
+    let _ = tokio::spawn(server);
+}
