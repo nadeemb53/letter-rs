e24fc3d76baa7d782060eff476266682138585b1
nadeemb53 <nadeemb53@gmail.com>
2022-08-22 18:30:29 +0530

    custom config support

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, App, HttpServer,};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
 
 use crate::routes;
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,8 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 
-fn spawn_app() -> String{
+fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to port.");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::startup::run(listener).expect("Failed to start server.");
@@ -9,11 +11,15 @@ fn spawn_app() -> String{
 }
 
 #[tokio::test]
-async fn health_check_works(){
+async fn health_check_works() {
     let address = spawn_app();
     let client = reqwest::Client::new();
 
-    let response = client.get(&format!("{}/health_check", &address)).send().await.expect("Failed to send request.");
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to send request.");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
@@ -21,18 +27,25 @@ async fn health_check_works(){
 
 #[tokio::test]
 async fn subscribe_returns_200_for_valid_form_data() {
-    //Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    // The `Connection` trait MUST be in scope for us to invoke
+    // `PgConnection::connect` - it is not an inherent method of the struct!
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
-    //Act
-    let body = "name=le%20&email=ursula_le_guine%40gmail.com";
-    let response = client.post(&format!("{}/subscriptions", &app_address))
-    .header("Content-Type", "application/x-www-form-urlencoded")
-    .body(body)
-    .send()
-    .await
-    .expect("Failed to execute request.");
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
 
     // Assert
     assert_eq!(200, response.status().as_u16());
@@ -46,17 +59,23 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guine%40gmail.com", "missing the name"),
-        ("","missing both name and email")
+        ("", "missing both name and email"),
     ];
     for (body, expected_message) in test_cases {
         // Act
-        let response = client.post(&format!("{}/subscriptions", &app_address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
         // Assert
-        assert_eq!(400, response.status().as_u16(), "Expected a 400 response for test case: {}", expected_message);
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "Expected a 400 response for test case: {}",
+            expected_message
+        );
     }
 }
