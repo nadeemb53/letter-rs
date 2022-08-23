06cf8b5342ca1d6ddde04089fcf473defe36c7ba
nadeemb53 <nadeemb53@gmail.com>
2022-08-23 12:38:43 +0530

    better logs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
