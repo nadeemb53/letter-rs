3b7801d5e682b342d116de1c5abaccb7f9e2d579
nadeemb53 <nadeemb53@gmail.com>
2022-08-23 14:28:06 +0530

    fmt fix

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -19,9 +19,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         subscriber_name = %form.name
     );
     let _request_span_guard = request_span.enter();
-    let query_span = tracing::info_span!(
-        "Saving new subscriber details in the database"
-    );
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
