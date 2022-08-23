08c5d86eac3d3940534eb6631a0039874a5d1184
nadeemb53 <nadeemb53@gmail.com>
2022-08-23 10:27:22 +0530

    applied migration

--- migrations/20220822114919_create_subscriptions_table.sql
+++ migrations/20220822114919_create_subscriptions_table.sql
@@ -1,1 +1,10 @@
 -- Add migration script here
+-- migrations/{timestamp}_create_subscriptions_table.sql
+-- Create Subscriptions Table
+CREATE TABLE subscriptions(
+   id uuid NOT NULL,
+   PRIMARY KEY (id),
+   email TEXT NOT NULL UNIQUE,
+   name TEXT NOT NULL,
+   subscribed_at timestamptz NOT NULL
+);
